41a0d347de1e151e2cfbc21109c5358b6ee3cf97
abhij <abhij@ABHIJITHTP>
2026-02-19 08:53:45 +0530

    Add project files.

---
+++ MVC_Sample_Application/Controllers/DBInsertController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Sample_Application.Models;
+
+namespace MVC_Sample_Application.Controllers
+{
+    public class DBInsertController : Controller
+    {
+        MVC_Sample_ApplicationEntities dbobj = new MVC_Sample_ApplicationEntities();
+        // GET: DBInsert
+        public ActionResult pageload()
+        {
+            return View();
+        }
+        public ActionResult ButtonClick(Insert clsobj)
+        {
+            if (ModelState.IsValid)
+            {
+                dbobj.sp_insert(clsobj.Name, clsobj.Age, clsobj.Email);
+                clsobj.Message = "insert succfully";
+                return View("pageload", clsobj);
+            }
+            return View("pageload", clsobj);
+        }
+
+    }
+}
---
+++ MVC_Sample_Application/Models/Insert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Sample_Application.Models
+{
+    public class Insert
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="enter the Name")]
+        public string Name { get; set; }
+        [Range(10,100,ErrorMessage ="enter the age")]
+        public int Age { get; set; }
+        [EmailAddress(ErrorMessage ="enter the email address")]
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
